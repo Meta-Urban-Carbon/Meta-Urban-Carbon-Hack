dc5c30ff4c7489a329b01925b4ee41adb3504bf7
Pragya Gupta <pragyagupta171@gmail.com>
2023-06-24 15:30:05 -0700

    adding a program class

--- class_setup.py
+++ class_setup.py
@@ -23,9 +23,71 @@ class buildingProgram:
         self.name = name
         self.type = type
         self.area = area
+        self.areaUnits = "ftSQ"
+        self.country = "USA",
+        self.postalCode: "60190"
+        self.state = "IL"
+        self.reportingUnits = "us"
+
 
     def __str__(self):
         return f"{self.name} {self.path}"
 
     def __repr__(self):
         return f"{self.name} {self.path}"
+    
+    def areaForProgram (self):
+        return print("area for " + self.type + " is " + self.area)
+
+    def baselineEUI (self):
+        dummyEUI = int(100)
+        return dummyEUI
+    
+    def baselineEnergyConsumption (self):
+        programEnergyConsumption = int(self.area) * int(self.baselineEUI())
+        return print("baseline energy consumption for " + self.type + " is", programEnergyConsumption)
+    
+    
+
+    
+    # def baselineEUI(self):
+        
+
+programTest = buildingProgram("Workspace", "Office", "1000")
+programTest.baselineEnergyConsumption()
+
+# import requests
+# import json
+
+def buildZeroToolDataInput():
+    return ""
+    
+    # data_input = {
+    #     'buildingType' = program.Type,
+    #     'GFA': above_ip + below_ip,
+    #     'areaUnits': 'ftSQ',
+    #     'country': 'USA',
+    #     'postalCode': project_zipcode,
+    #     'state': project_state_code,
+    #     'city': project_city,
+    #     'reportingUnits': 'us'
+    # }
+
+    # {"buildingType": "Office","GFA": 123123,"areaUnits": "ftSQ","country": "USA","postalCode": "60190","state": "IL","reportingUnits": "us"}
+    # return data_input
+
+# def getZeroToolData(project_basics, project_details):
+#     data_input_objects = []
+#     for i in range(len(project_details['buildingFloors'])):
+#         data_input_objects.append(buildZeroToolDataInputObject(project_basics, project_details, i))
+
+#     requests_responses = []
+#     for data in data_input_objects:
+#         response = requests.post('https://tool.zerotool.org/baseline', 
+#                                  headers={'Accept': 'application/json', 'Content-Type': 'application/json'},
+#                                  data=json.dumps([data]))
+#         requests_responses.append(response.json())
+        
+#     return requests_responses
+
+

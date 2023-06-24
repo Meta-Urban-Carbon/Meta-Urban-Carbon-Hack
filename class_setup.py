ac4851de3638f96597188d39d0c66e0535fdb302
Pragya Gupta <pragyagupta171@gmail.com>
2023-06-24 14:18:46 -0700

    Creating Project class

---
+++ class_setup.py
@@ -0,0 +1,20 @@
+class project:
+    def __init__(self, name, path, zipcode, moveInYear):
+        self.name = name
+        self.path = path
+        self.zipcode = zipcode
+        self.moveInYear = moveInYear
+        self.operationalYears = 50
+        self.unit = "ip"
+
+
+    def __str__(self):
+        return f"{self.name} {self.path}"
+
+    def __repr__(self):
+        return f"{self.name} {self.path}"
+    
+    def cityState (self, zipcode):
+        return print("My zip code is " + zipcode)
+    
+    
